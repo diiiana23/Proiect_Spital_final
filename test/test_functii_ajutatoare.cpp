2aab6975801ca0c68287910ebd7c3632d208ffdd
diiiana23 <diana.alexandrescu23@gmail.com>
2025-05-26 02:29:27 +0300

    testele14

--- test/test_functii_ajutatoare.cpp
+++ test/test_functii_ajutatoare.cpp
@@ -1,54 +1,54 @@
-// #include "gtest/gtest.h"
-// #include "FunctiiAjutatoare.h"
-// #include "Pacient.h"
-//
-// class TestPacient : public Pacient {
-//     int grad;
-// public:
-//     TestPacient(int grad_)
-//         : Pacient("Nume", "Prenume", "1234567890123", 'F', 30, "01.01.1994",
-//                   "Bucuresti", 2, "romana", "programator", "universitar",
-//                   "A", "+", "nu", "da"),
-//           grad(grad_) {}
-//
-//     void procesareSpeciala() const override {}
-//
-//     int gradProblema() const override {
-//         return grad;
-//     }
-// };
-//
-// TEST(FunctiiAjutatoareTest, CalculezCostulValoriCorecte) {
-//     EXPECT_EQ(FunctiiAjutatoare::calculezCostul(1), 150);
-//     EXPECT_EQ(FunctiiAjutatoare::calculezCostul(3), 300);
-//     EXPECT_EQ(FunctiiAjutatoare::calculezCostul(5), 600);
-// }
-//
-// TEST(FunctiiAjutatoareTest, CalculezCostulValoriInvalide) {
-//     EXPECT_EQ(FunctiiAjutatoare::calculezCostul(0), 0);
-//     EXPECT_EQ(FunctiiAjutatoare::calculezCostul(-5), 0);
-//     EXPECT_EQ(FunctiiAjutatoare::calculezCostul(9), 0);
-// }
-//
-// TEST(FunctiiAjutatoareTest, ZileInternareCorecte) {
-//     TestPacient p1(1);
-//     TestPacient p3(3);
-//     TestPacient p5(5);
-//
-//     EXPECT_EQ(FunctiiAjutatoare::zileInternare(p1), 0);
-//     EXPECT_EQ(FunctiiAjutatoare::zileInternare(p3), 2);
-//     EXPECT_EQ(FunctiiAjutatoare::zileInternare(p5), 4);
-// }
-//
-// TEST(FunctiiAjutatoareTest, SpecializareProblemaCorecta) {
-//     EXPECT_EQ(FunctiiAjutatoare::Specializare_Problema("Pneumonie"), "Pneumologie");
-//     EXPECT_EQ(FunctiiAjutatoare::Specializare_Problema("Infectie urinara"), "Nefrologie");
-//     EXPECT_EQ(FunctiiAjutatoare::Specializare_Problema("Durere lombara"), "Neurologie");
-//     EXPECT_EQ(FunctiiAjutatoare::Specializare_Problema("Ceva random"), "Medicina Generala");
-// }
-//
-// TEST(FunctiiAjutatoareTest, TrimEliminaSpatii) {
-//     std::string input = "   test   ";
-//     std::string output = FunctiiAjutatoare::trim(input);
-//     EXPECT_EQ(output, "test");
-// }
+#include "gtest/gtest.h"
+#include "FunctiiAjutatoare.h"
+#include "Pacient.h"
+
+class TestPacient : public Pacient {
+    int grad;
+public:
+    TestPacient(int grad_)
+        : Pacient("Test", "Pacient", "1234567890123", 'F', 30, "01.01.1994",
+                  "Bucuresti", 1, "romana", "test", "test",
+                  "A", "+", "nu", "da"), grad(grad_) {}
+
+    void procesareSpeciala() const override {}
+
+    int gradProblema() const override {
+        return grad;
+    }
+};
+
+TEST(FunctiiAjutatoareTest, CalculezCostulCorect) {
+    EXPECT_EQ(FunctiiAjutatoare::calculezCostul(1), 150);
+    EXPECT_EQ(FunctiiAjutatoare::calculezCostul(3), 300);
+    EXPECT_EQ(FunctiiAjutatoare::calculezCostul(5), 600);
+}
+
+TEST(FunctiiAjutatoareTest, CalculezCostulInvalid) {
+    EXPECT_EQ(FunctiiAjutatoare::calculezCostul(0), 0);
+    EXPECT_EQ(FunctiiAjutatoare::calculezCostul(-2), 0);
+    EXPECT_EQ(FunctiiAjutatoare::calculezCostul(99), 0);
+}
+
+TEST(FunctiiAjutatoareTest, ZileInternareInFunctieDeGrad) {
+    TestPacient p1(1), p2(2), p3(3), p4(4), p5(5);
+
+    EXPECT_EQ(FunctiiAjutatoare::zileInternare(p1), 0);
+    EXPECT_EQ(FunctiiAjutatoare::zileInternare(p2), 2);
+    EXPECT_EQ(FunctiiAjutatoare::zileInternare(p3), 2);
+    EXPECT_EQ(FunctiiAjutatoare::zileInternare(p4), 4);
+    EXPECT_EQ(FunctiiAjutatoare::zileInternare(p5), 4);
+}
+
+TEST(FunctiiAjutatoareTest, SpecializareCorectaPentruDiagnostic) {
+    EXPECT_EQ(FunctiiAjutatoare::Specializare_Problema("Pneumonie"), "Pneumologie");
+    EXPECT_EQ(FunctiiAjutatoare::Specializare_Problema("Hipotiroidism"), "Endocrinologie");
+    EXPECT_EQ(FunctiiAjutatoare::Specializare_Problema("Durere lombara"), "Neurologie");
+    EXPECT_EQ(FunctiiAjutatoare::Specializare_Problema("Infectie urinara"), "Nefrologie");
+    EXPECT_EQ(FunctiiAjutatoare::Specializare_Problema("Ceva random"), "Medicina Generala");
+}
+
+TEST(FunctiiAjutatoareTest, EliminareSpatii) {
+    EXPECT_EQ(FunctiiAjutatoare::trim("   abc   "), "abc");
+    EXPECT_EQ(FunctiiAjutatoare::trim("test"), "test");
+    EXPECT_EQ(FunctiiAjutatoare::trim("      "), "");
+}
