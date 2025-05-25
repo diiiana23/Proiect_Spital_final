ef8a9b99905fe34f558e2872359824dc21d27e2a
diiiana23 <diana.alexandrescu23@gmail.com>
2025-05-25 23:44:26 +0300

    teste2

--- test/test_donator.cpp
+++ test/test_donator.cpp
@@ -1,7 +1,7 @@
-// #include "gtest/gtest.h"
-// #include "Donator.h"
-//
-// TEST(DonatorTest, FormatCorect) {
-//     Donator d("Ana", "Popescu", 30, "A+");
-//     EXPECT_EQ(d.format(), "Ana Popescu, 30, A+");
-// }
+#include "gtest/gtest.h"
+#include "Donator.h"
+
+TEST(DonatorTest, FormatCorect) {
+    Donator d("Ana", "Popescu", 30, "A+");
+    EXPECT_EQ(d.format(), "Ana Popescu, 30, A+");
+}
