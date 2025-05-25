600d86e5a68cd23cb026de23342665884aafe2a2
diiiana23 <diana.alexandrescu23@gmail.com>
2025-05-26 01:01:51 +0300

    teste3

--- test/test_unitate_medicala.cpp
+++ test/test_unitate_medicala.cpp
@@ -1,19 +1,19 @@
-// #include "gtest/gtest.h"
-// #include "UnitateMedicala.h"
-//
-// // verific daca functioneaza fct de GET
-//
-// TEST(UnitateMedicalaTest, TestGetNume) {
-//     UnitateMedicala spital("Spitalul Regina-Maria", 100, 25);
-//     EXPECT_EQ(spital.getNume(), "Spitalul Regina-Maria");
-// }
-//
-// TEST(UnitateMedicalaTest, TestGetNrPaturi) {
-//     UnitateMedicala spital("Spitalul Regina-Maria", 100, 25);
-//     EXPECT_EQ(spital.getNrPaturi(), 100);
-// }
-//
-// TEST(UnitateMedicalaTest, TestGetNrDoctori) {
-//     UnitateMedicala spital("Spitalul Regina-Maria", 100, 25);
-//     EXPECT_EQ(spital.getNrDoctori(), 25);
-// }
+#include "gtest/gtest.h"
+#include "UnitateMedicala.h"
+
+// verific daca functioneaza fct de GET
+
+TEST(UnitateMedicalaTest, TestGetNume) {
+    UnitateMedicala spital("Spitalul Regina-Maria", 100, 25);
+    EXPECT_EQ(spital.getNume(), "Spitalul Regina-Maria");
+}
+
+TEST(UnitateMedicalaTest, TestGetNrPaturi) {
+    UnitateMedicala spital("Spitalul Regina-Maria", 100, 25);
+    EXPECT_EQ(spital.getNrPaturi(), 100);
+}
+
+TEST(UnitateMedicalaTest, TestGetNrDoctori) {
+    UnitateMedicala spital("Spitalul Regina-Maria", 100, 25);
+    EXPECT_EQ(spital.getNrDoctori(), 25);
+}
