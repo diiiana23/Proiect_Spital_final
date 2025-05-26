d14da0902d2031153749f9d95d76f817e720297e
diiiana23 <diana.alexandrescu23@gmail.com>
2025-05-26 15:11:50 +0300

    modificare script1

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-cppcheck --enable=all \
+cppcheck --enable=all --quiet \
     --inline-suppr \
     --project="${BUILD_DIR:-build}"/compile_commands.json \
+    -I "${BUILD_DIR:-build}/_deps/googletest-src/googletest/include" \
     -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
     -i"${EXT_DIR:-ext}" --suppress="*:${EXT_DIR:-ext}/*" \
     -i"${GEN_DIR:-generated}" --suppress="*:${GEN_DIR:-generated}/*" \
@@ -10,4 +11,3 @@ cppcheck --enable=all \
     --suppress=unmatchedSuppression \
     --suppress=useStlAlgorithm \
     --check-level=exhaustive \
-    --error-exitcode=1
